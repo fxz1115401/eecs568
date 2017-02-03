763ba5b2c5a46ef06080a9883ada4141fe34c514
xinzhiFan <xfanac@umich.edu>
2017-02-03 13:42:49 -0500

    task 1 finish

--- hw1/task1.m
+++ hw1/task1.m
@@ -4,11 +4,13 @@ close all
 X=(0:0.01:5);
 Y=normpdf(X,1,1);
 plot(X,Y)
+axis equal
 figure
 
 %%% part b %%%
 Y1=normcdf(X,1,1);
 plot(X,Y1)
+axis equal
 figure
 
 %%% part c %%%
@@ -18,10 +20,16 @@ p_z = integral(fun,-Inf,Inf);
 p_xgivenz=normpdf(X,1,1).*normpdf(0.75,X,0.2^0.5)/p_z;
 subplot(2,1,1)
 plot(X,Y)
+axis equal
+
+
 subplot(2,1,2)
 plot(X,p_xgivenz)
+axis equal
+
 figure
 
 %%% part e %%%
 [x,z]=meshgrid(0:0.01:5);
 mesh(x,z,normpdf(x,1,1).*normpdf(z,x,0.2^0.5))
+axis equal
